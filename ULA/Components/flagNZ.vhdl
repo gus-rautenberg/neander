cad121c32f905a0a7d0d6c84b96a7076c1bee4b1
gus-rautenberg <gprautenberg@gmail.com>
2022-06-23 15:16:35 -0300

    NZ

--- ULA/Components/flagNZ.vhdl
+++ ULA/Components/flagNZ.vhdl
@@ -1,33 +1,29 @@
-library ieee;
-use ieee.std_logic_1164.all;
+#library ieee;
 
-entity flagnz is
-	port(
-		d      : in  std_logic_vector(1 downto 0);
-		clock  : in  std_logic;
-		pr, cl : in  std_logic;
-		nrw    : in  std_logic;
-		s      : out std_logic_vector(1 downto 0)
-	);
-end entity flagnz;
+entity flagNZ is
+    port(
+        clk, reset : std_logic;
+        dataIN : in std_logic_vector(1 downto 0);
+        ac_nrw : in std_logic;
+        dataOUT : out std_logic_vector(1 downto 0);
+    )
+end entity;
 
-architecture reg1bit of flagnz is
-
-	component regCarga1bit is
-		port(
-			d      : in  std_logic;
-			clock  : in  std_logic;
-			pr, cl : in  std_logic;
-			nrw    : in  std_logic;
-			s      : out std_logic
-		);
-	end component regCarga1bit;
-
-    -- signal ref_d, s_d : std_logic_vector(1 downto 0);
-begin
+architecture status of flagNZ is
+    component ffd is
+        port(
+            dataIN : std_logic;
+            clk, pr, cl : std_logic;
+            dataout: std_logic
+        );
+end component
+ signal ref_d, s_d : std_logic_vector(1 downto 0);
+ begin
     --flag N
-    u_ffdN : regCarga1bit port map(d(1), clk, pr, cl, nrw, s(1));
-    -- flag Z
-    u_ffZ  : regCarga1bit port map(d(0), clk, cl, pr, nrw, s(0));
+    u_ffdN : ffd port map(reg_d, clk, '1', reset, s_d(1));
+    u_ffZ: ffd port map(reg_d(0), clk, reset, '1', s_d(0));
+
+    --mux
+    dataout <= s_d;
 
 end architecture
