d66d3ca81fa209cb3b8bbc5845ee9ec83062e4fe
Joao-Biederman <joaobiedetman@gmail.com>
2022-06-23 12:53:40 -0300

    AC att

--- ULA/Components/AC.vhdl
+++ ULA/Components/AC.vhdl
@@ -1,7 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity regCarga8bit is
+entity ac is
 	port(
 		d      : in  std_logic_vector(7 downto 0);
 		clock  : in  std_logic;
@@ -9,9 +9,9 @@ entity regCarga8bit is
 		nrw    : in  std_logic;
 		s      : out std_logic_vector(7 downto 0)
 	);
-end entity regCarga8bit;
+end entity ac;
 
-architecture reg1bit of regCarga8bit is
+architecture reg1bit of ac is
 
 	component regCarga1bit is
 		port(
@@ -27,10 +27,10 @@ architecture reg1bit of regCarga8bit is
 begin
 	
 	
-	gen_regCarga8bit : for i in 0 to 7 generate
+	gen_ac : for i in 0 to 7 generate
 		
 		u_reg : regCarga1bit port map(d(i), clock, pr, cl, nrw, s(i));
 
-	end generate gen_regCarga8bit;
+	end generate gen_ac;
 	
 end architecture;
