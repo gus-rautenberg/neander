cb5d37cb9da3b15ad5185e0a72d1802606663bc9
gus-rautenberg <gprautenber@gmail.com>
2022-07-24 20:42:14 -0300

    Neander

--- ctrl/mod_OR.vhdl
+++ ctrl/mod_OR.vhdl
@@ -1,29 +1,29 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity mod_LDA is
+entity mod_OR is
     port(
         b : in std_logic_vector(2 downto 0);
-        controle : out std_logic_vector(10 downto 0);
+        controle : out std_logic_vector(10 downto 0)
     );
 end entity;
 
-architecture bhvr of mod_LDA is
+architecture bhvr of mod_OR is
 begin
 
     -- Barr
-    controle(0) <= 1;
-    controle(1) <= not(b(2)) or b(1) or not(b(0));
+    controle(10) <= '1';
+    controle(9) <= not(b(2)) or b(1) or not(b(0));
     -- OP
-    controle(2) <= 0;
-    controle(3) <= 1;
-    controle(4) <= 0;
+    controle(8) <= '0';
+    controle(7) <= '1';
+    controle(6) <= '0';
     -- !rw
     controle(5) <= not(b(1)) and (b(2) xor b(0)); -- PC
-    controle(6) <= b(2) and b(1) and b(0); -- AC
-    controle(7) <= 0; -- MEM
-    controle(8) <= (not(b1) and (b(2) xnor b(0))) or (not(b(2)) and b(1) and b(0)); -- REM
-    controle(9) <= (b2 and not(b0)) or (not(b(2)) and not(b(1)) and b(0)); -- RDM
-    controle(10) <= not(b(2)) and b(1) and not(b(0)); -- RI
+    controle(4) <= b(2) and b(1) and b(0); -- AC
+    controle(3) <= '0'; -- MEM
+    controle(2) <= (not(b(1)) and (b(2) xnor b(0))) or (not(b(2)) and b(1) and b(0)); -- REM
+    controle(1) <= (b(2) and not(b(0))) or (not(b(2)) and not(b(1)) and b(0)); -- RDM
+    controle(0) <= not(b(2)) and b(1) and not(b(0)); -- RI
 
 end architecture;
