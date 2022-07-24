27a552b9cb1b56a220b57531dc39b89e298537c6
Joao-Biederman <joaobiedetman@gmail.com>
2022-07-24 17:09:14 -0300

    NOP LDA e os krl

--- ctrl/mod_NOP.vhdl
+++ ctrl/mod_NOP.vhdl
@@ -12,18 +12,18 @@ architecture bhvr of mod_NOP is
 begin
 
     -- Barr
-    controle(0) <= 0;
-    controle(1) <= 0;
+    controle(0) <= 1;
+    controle(1) <= 1;
     -- OP
     controle(2) <= 0;
     controle(3) <= 0;
     controle(4) <= 0;
     -- !rw
-    controle(5) <= 0; -- PC
+    controle(5) <= not(b(2)) and not(b(1)) and b(0); -- PC
     controle(6) <= 0; -- AC
     controle(7) <= 0; -- MEM
-    controle(8) <= 0; -- REM
-    controle(9) <= 0; -- RDM
+    controle(8) <= not(b(2)) and not(b(1)) and not(b(0)); -- REM
+    controle(9) <= not(b(2)) and not(b(1)) and b(0); -- RDM
     controle(10) <= 0; -- RI
 
 end architecture;
